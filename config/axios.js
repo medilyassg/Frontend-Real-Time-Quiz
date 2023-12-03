d946ee62af2ab010282d43fcccfb0a5cbdd5ebc0
Youssef Dalouji <daloujiy@gmail.com>
2023-12-03 02:42:14 +0000

    Add Tailwind & Config Auth

---
+++ config/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  
+    withCredentials: true,
+  });
+
+  export {api}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import './index.css';
+import { api } from '../config/axios';
+import { useEffect } from 'react';
 function App() {
-  
+  useEffect(() => {
+    getUser();
+  }, [])
+  async function getUser(){
+    const csrf=await api.get('/sanctum/csrf-cookie')
+
+    console.log('csrf : ',csrf)
+
+    const login = await api.post('/api/login',{
+      email:'jovani24@example.com',
+      password:'password'
+    })
+
+    console.log('login : ',login)
+
+    const user=await api.get('/api/user')
+    console.log('user : ',user)
+  }
+  return <>
+    <h1 className="text-3xl font-bold underline bg-red-600">
+      Hello world!
+    </h1>
+  </>
 }
 
 export default App
