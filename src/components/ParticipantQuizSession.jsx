588e118d08c534960760e389d428774276f3bb1e
ayoub <ayoubaarab571@gmail.com>
2023-12-08 01:50:19 +0100

    modify the participant session

--- src/components/ParticipantQuizSession.jsx
+++ src/components/ParticipantQuizSession.jsx
@@ -8,26 +8,17 @@ import { RiRectangleFill } from 'react-icons/ri';
 
 const ParticipantQuizSession = () => {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [timer, setTimer] = useState(15);
-  const [showResult, setShowResult] = useState(false);
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+  const [timer, setTimer] = useState(5);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const navigate = useNavigate();
 
   const handleAnswerClick = (index) => {
-    // Check if a response is already selected, and ignore the click if it is
-    if (selectedAnswer !== null) {
+    // Check if the timer has run out or the answer is already selected, and ignore the click if true
+    if (timer === 0 || selectedAnswer !== null) {
       return;
     }
 
     setSelectedAnswer(index);
-    setShowResult(true);
-    setIsAnswerCorrect(true);
-    setTimeout(() => {
-      setShowResult(false);
-      setSelectedAnswer(null);
-      setTimer(15);
-      navigate('/ParticipantWaiting');
-    }, 2000);
   };
 
   useEffect(() => {
@@ -36,7 +27,7 @@ const ParticipantQuizSession = () => {
     }, 1000);
 
     // Launch the waiting page after the timer reaches 0
-    if (timer === 0) {
+    if (timer === 0 && selectedAnswer !== null) {
       clearInterval(interval);
       navigate('/ParticipantWaiting');
     }
@@ -44,29 +35,53 @@ const ParticipantQuizSession = () => {
     return () => {
       clearInterval(interval);
     };
+  }, [timer, navigate, selectedAnswer]);
+
+  useEffect(() => {
+    // Disable navigation until the timer runs out
+    const timeoutNavigation = setTimeout(() => {
+      navigate('/ParticipantWaiting');
+    }, timer * 1000);
+
+    return () => {
+      clearTimeout(timeoutNavigation);
+    };
   }, [timer, navigate]);
 
   const answerColors = ['bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-red-500'];
-  const answerIcons = [<FaStar size={32} />, <IoTriangle size={32} />, <FaCircle size={32} />, <RiRectangleFill size={32} />];
+  const answerIcons = [<FaStar size={24} />, <IoTriangle size={24} />, <FaCircle size={24} />, <RiRectangleFill size={24} />];
+
+  const questions = [
+    {
+      question: 'What is the capital of France?',
+      answers: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+    },
+    {
+      question: 'What is the capital of Uruguay?',
+      answers: ['Berlin', 'Montevideo', 'Paris', 'Rome'],
+    },
+  ];
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-2xl mx-auto p-4 bg-white shadow-lg rounded-md">
+      <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-md">
+        {/* Question outside the box */}
+        <h2 className="text-3xl font-bold mb-4 text-center">
+          Question {currentQuestionIndex + 1}: {questions[currentQuestionIndex].question}
+        </h2>
         <div className="grid grid-cols-2 gap-4">
           {answerIcons.map((icon, index) => (
             <div
               key={index}
-              className={`p-4 text-4xl font-bold rounded-md ${answerColors[index]} text-white transition-all duration-300 transform hover:scale-105 hover:shadow-lg cursor-pointer ${
+              className={`flex items-center p-4 text-lg font-bold rounded-md ${answerColors[index]} text-white transition-all duration-300 transform hover:scale-105 hover:shadow-lg cursor-pointer ${
                 selectedAnswer === index ? 'border-2 border-black' : 'border-2 border-transparent'
               }`}
               onClick={() => handleAnswerClick(index)}
             >
-              <div className="flex items-center justify-center space-x-3">
-                <div className="flex items-center justify-center h-16 w-16 rounded-full bg-gray-200 mb-2">
-                  {icon}
-                </div>
-                <div>{String.fromCharCode(65 + index)}</div>
+              <div className={`rounded-full h-12 w-12 flex items-center justify-center mr-4 ${selectedAnswer === index ? 'bg-gray-200' : ''}`}>
+                {icon}
               </div>
+              <div>{questions[currentQuestionIndex].answers[index]}</div>
             </div>
           ))}
         </div>
