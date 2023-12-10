31c6ef89dcdf5f9f067b557d81e6e2d79442d024
imorabet <mrabetikhlas700@gmail.com>
2023-12-10 20:17:46 +0100

    add the home page ß(

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,37 +1,63 @@
-import { api } from '../../config/axios';
-import { useEffect } from 'react';
-import { useDispatch,useSelector } from "react-redux";
-import {addAuthData,isLogin} from '../store/authDataReducer'
+import { api } from "../../config/axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addAuthData, isLogin } from "../store/authDataReducer";
+import {
+  Avatar,
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
+import { Link } from "react-router-dom";
 
-const LoginPage=()=>{
-    const dispatch=useDispatch();
-    let dataUser=useSelector(state=>state);
-    
-    useEffect(() => {
-        getUser();
-      }, [])
-      
-      async function getUser(){
-        const csrf=await api.get('/sanctum/csrf-cookie')
-    
-        const login = await api.post('/api/v1/auth/login',{
-          email:'jovani24@example.com',
-          password:'password'
-        })
-    
-        const user=await api.get('/api/v1/user')
-        if(user.status>=200 && user.status<300){
-            dispatch(addAuthData(user.data.data))
-            dispatch(isLogin())
-        }
-      }
-      console.log(dataUser)
-      
-      return <>
-        <h1 className="text-3xl font-bold underline bg-red-600">
-          Hello world!
-        </h1>
-      </>
-}
+const LoginPage = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default LoginPage
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+  const dispatch = useDispatch();
+  let dataUser = useSelector((state) => state);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  async function getUser() {
+    const csrf = await api.get("/sanctum/csrf-cookie");
+
+    const login = await api.post("/api/v1/auth/login", {
+      email: "jovani24@example.com",
+      password: "password",
+    });
+
+    const user = await api.get("/api/v1/user");
+    if (user.status >= 200 && user.status < 300) {
+      dispatch(addAuthData(user.data.data));
+      dispatch(isLogin());
+    }
+  }
+  console.log(dataUser);
+
+  return (
+    <>
+    
+       </>
+  );
+};
+
+export default LoginPage;
