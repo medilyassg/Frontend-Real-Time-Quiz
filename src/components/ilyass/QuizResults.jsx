461310fc2491b9b471bdf8c130fb5c65e0d4876f
medilyassg <elghazi.mohamed.ilyass@gmail.com>
2023-12-12 11:04:35 +0100

    Add the excel export for the score table

--- src/components/ilyass/QuizResults.jsx
+++ src/components/ilyass/QuizResults.jsx
@@ -4,7 +4,21 @@ import * as XLSX from 'xlsx';
 
 const QuizResults = ({ participants }) => {
   const exportToExcel = () => {
-    const worksheet = XLSX.utils.json_to_sheet(participants);
+    // Create a custom worksheet for export
+    const worksheet = XLSX.utils.json_to_sheet(
+      participants.map((participant, index) => ({
+        id: index + 1,
+        name: participant.name,
+        Q1: participant.results[0] ? 'Correct' : 'Incorrect',
+        Q2: participant.results[1] ? 'Correct' : 'Incorrect',
+        Q3: participant.results[2] ? 'Correct' : 'Incorrect',
+        Q4: participant.results[3] ? 'Correct' : 'Incorrect',
+      }))
+    );
+
+    // Set column headers
+    XLSX.utils.sheet_add_aoa(worksheet, [['ID', 'Name', 'Q1', 'Q2', 'Q3', 'Q4']], { origin: 'A1' });
+
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'QuizResults');
     XLSX.writeFile(workbook, 'quiz_results.xlsx');
@@ -12,14 +26,13 @@ const QuizResults = ({ participants }) => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b bg-gradient-to-tr from-blue-400 to-cyan-200 relative">
-
-
       <div className="bg-white p-8 border border-blue-900 rounded-md shadow-md w-full max-w-xxl mx-3 relative">
         <div className="absolute top-0 right-0 mt-4 mr-4">
-
-          <button type="button"
+          <button
+            type="button"
             onClick={exportToExcel}
-            className=" text-1xl font-bold text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full  p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+            className="text-1xl font-bold text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"
+          >
             <IoIosDownload />
             <span className="sr-only">Icon description</span>
           </button>
@@ -27,7 +40,6 @@ const QuizResults = ({ participants }) => {
         <div className="mb-4">
           <h2 className="text-1xl font-bold text-blue-500">Quiz Results</h2>
         </div>
-        
         <div className="overflow-x-auto">
           <table className="min-w-full border bg-white">
             <thead>
@@ -43,11 +55,11 @@ const QuizResults = ({ participants }) => {
             </thead>
             <tbody>
               {participants.map((participant, index) => (
-                <tr key={index} className=" text-center">
+                <tr key={index} className="text-center">
                   <td className="border p-2">{index + 1}</td>
                   <td className="border p-2">{participant.name}</td>
                   {participant.results.map((result, index) => (
-                    <td key={index} className={`border p-2 ${result ? 'bg-green-100' : 'bg-red-100'} `}>
+                    <td key={index} className={`border p-2 ${result ? 'bg-green-100' : 'bg-red-100'}`}>
                       {result ? <IoIosCheckmark className="text-green-500 text-2xl text-center" /> : <IoIosClose className="text-red-500 text-lg" />}
                     </td>
                   ))}
